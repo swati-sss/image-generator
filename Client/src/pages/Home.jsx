1e58495c9a8b5b4094192ff4b0d86a0f46e22572
Swati Srivastava <62814696+swati-sss@users.noreply.github.com>
2024-01-31 00:28:47 -0800

    Update Home.jsx

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
     const fetchposts = async ()=> {
       setLoading(true);
       try{
-        const response = await fetch('https://my-ai-image-generator.onrender.com/api/v1/post', 
+        const response = await fetch('https://image-generator-p3xu.onrender.com/api/v1/post', 
         {
           method : 'GET',
           headers : {
