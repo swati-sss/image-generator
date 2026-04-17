391c399a7020748c54e2b16d8720adc90f55d075
Swati Srivastava <62814696+swati-sss@users.noreply.github.com>
2026-04-17 14:45:29 -0500

    Update BlueDotVerificationTests.swift

--- Dot/BlueDotVerificationTests.swift
+++ Dot/BlueDotVerificationTests.swift
@@ -1,48 +1,70 @@
 //
-//  SecureKeys.h
+//  SecureKeys.m
 //  WalmartPlatformExtensions
 //
 //  Created by Venkatesh Jujjavarapu on 01/12/20.
 //  Copyright © 2020 Walmart. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import "SecureKeys.h"
 
-extern NSString* const kBrazeAPIKey;
-extern NSString* const kPerimeterXAPIKey;
-extern NSString* const kPerimeterXAPIDebugKey;
-extern NSString* const kQuantumMetricKey;
-extern NSString* const kQuantumMetricStageKey;
-extern NSString* const kAppsFlyerDevKey;
-extern NSString* const kAppsFlyerAppID;
-extern NSString* const kAppsFlyerOneLinkAuth;
-extern NSString* const kFitPredictorServiceKey;
-extern NSString* const kFitPredictorPartnerKey;
-extern NSString* const kFitPredictorEmailSalt;
-extern NSString* const kFlippAPIKey;
-extern NSString* const kThreatMetrixStgOrgID;
-extern NSString* const kThreatMetrixProdOrgID;
-extern NSString* const kEncryptionKey;
-extern NSString* const kIdMeClientIdKey; 
-extern NSString* const kAppCenterAPIKey;
-extern NSString* const kArAPIClientIdKey;
-extern NSString* const kArAPIClientSecretKey;
-extern NSString* const kFisPaypageIDKey;
-extern NSString* const kFisPaypageIDProdKey;
-extern NSString* const kBazaarvoiceLicenceKey;
-extern NSString* const ktslSharedSecrete;
-extern NSString* const ktslSharedDevSecrete;
-extern NSString* const ktslClientKey;
-extern NSString* const ktslClientDevKey;
-extern NSString* const kLivestreamSaltKey;
-extern NSString* const klivestreamSaltDevKey;
-extern NSString* const karopticalProdKey;
-extern NSString* const karopticalDevKey;
-extern NSString* const kDSGNativePartnerKey;
-extern NSString* const kDSGNativeAPIServiceKey;
-extern NSString* const kCompassProdLoggedInConsumerIdKey;
-extern NSString* const kCompassStageLoggedInConsumerIdKey;
-extern NSString* const kCompassProdGuestConsumerIdKey;
-extern NSString* const kCompassStageGuestConsumerIdKey;
-extern NSString* const kCompassProdGuestClientSecretKey;
-extern NSString* const kCompassStageGuestClientSecretKey;
+NSString *const kBrazeAPIKey = @OS_STRINGIFY(BRAZE_KEY);
+
+NSString *const kPerimeterXAPIKey = @OS_STRINGIFY(PERIMETERX_KEY);
+
+NSString *const kPerimeterXAPIDebugKey = @OS_STRINGIFY(PERIMETERX_KEY_DEBUG);
+
+NSString *const kQuantumMetricKey = @OS_STRINGIFY(QUANTUMMETRIC_UID);
+
+NSString *const kQuantumMetricStageKey = @OS_STRINGIFY(QUANTUMMETRIC_STAGE_UID);
+
+NSString *const kAppsFlyerDevKey = @OS_STRINGIFY(APPSFLYER_DEV_KEY);
+
+NSString *const kAppsFlyerAppID = @OS_STRINGIFY(APPSFLYER_APP_ID);
+
+NSString *const kAppsFlyerOneLinkAuth = @OS_STRINGIFY(APPSFLYER_ONELINK_AUTH);
+
+NSString *const kFitPredictorPartnerKey = @OS_STRINGIFY(FIT_PREDICTOR_PARTNER_KEY);
+
+NSString *const kFitPredictorServiceKey = @OS_STRINGIFY(FIT_PREDICTOR_SERVICE_KEY);
+
+NSString *const kFitPredictorEmailSalt = @OS_STRINGIFY(FIT_PREDICTOR_EMAIL_SALT);
+
+NSString *const kFlippAPIKey = @OS_STRINGIFY(FLIPP_KEY);
+
+NSString *const kThreatMetrixStgOrgID = @OS_STRINGIFY(THREATMETRIX_STG_ORG_ID);
+
+NSString *const kThreatMetrixProdOrgID = @OS_STRINGIFY(THREATMETRIX_PROD_ORG_ID);
+
+NSString *const kEncryptionKey = @OS_STRINGIFY(ENCRYPTION_KEY);
+
+NSString *const kIdMeClientIdKey = @OS_STRINGIFY(ID_ME_CLIENT_ID_KEY);
+
+NSString *const kAppCenterAPIKey = @OS_STRINGIFY(APPCENTER_KEY);
+
+NSString* const kArAPIClientIdKey = @OS_STRINGIFY(AR_API_CLIENT_KEY);
+
+NSString* const kArAPIClientSecretKey = @OS_STRINGIFY(AR_API_CLIENT_SECRET_KEY);
+
+NSString* const kFisPaypageIDKey = @OS_STRINGIFY(FIS_PAYPAGEID_KEY);
+
+NSString* const kFisPaypageIDProdKey = @OS_STRINGIFY(FIS_PAYPAGEID_PROD_KEY);
+
+NSString *const kBazaarvoiceLicenceKey = @OS_STRINGIFY(BAZAARVOICE_KEY);
+
+NSString *const ktslSharedDevSecrete = @OS_STRINGIFY(TSL_SECRETE_DEV);
+NSString *const ktslSharedSecrete = @OS_STRINGIFY(TSL_SECRETE);
+NSString *const ktslClientDevKey = @OS_STRINGIFY(TSL_KEY_DEV);
+NSString *const ktslClientKey = @OS_STRINGIFY(TSL_KEY);
+NSString *const klivestreamSaltDevKey = @OS_STRINGIFY(LIVESTREAM_SALT_DEV);
+NSString *const kLivestreamSaltKey = @OS_STRINGIFY(LIVESTREAM_SALT);
+NSString *const karopticalProdKey = @OS_STRINGIFY(AROPTICAL_PROD);
+NSString *const karopticalDevKey = @OS_STRINGIFY(AROPTICAL_DEV);
+NSString *const kDSGNativePartnerKey = @OS_STRINGIFY(DSG_NATIVE_PARTNER_KEY);
+NSString *const kDSGNativeAPIServiceKey = @OS_STRINGIFY(DSG_NATIVE_PARTNER_SERVICE_KEY);
+NSString *const kCompassProdLoggedInConsumerIdKey = @OS_STRINGIFY(COMPASS_PROD_LOGGED_IN_CONSUMER_ID);
+NSString *const kCompassStageLoggedInConsumerIdKey = @OS_STRINGIFY(COMPASS_STAGE_LOGGED_IN_CONSUMER_ID);
+NSString *const kCompassProdGuestConsumerIdKey = @OS_STRINGIFY(COMPASS_PROD_GUEST_CONSUMER_ID);
+NSString *const kCompassStageGuestConsumerIdKey = @OS_STRINGIFY(COMPASS_STAGE_GUEST_CONSUMER_ID);
+NSString *const kCompassProdGuestClientSecretKey = @OS_STRINGIFY(COMPASS_PROD_GUEST_CLIENT_SECRET);
+NSString *const kCompassStageGuestClientSecretKey = @OS_STRINGIFY(COMPASS_STAGE_GUEST_CLIENT_SECRET);
