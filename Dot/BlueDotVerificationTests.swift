fa9177cae1b570d887feca68d174c559e4f0e2b3
Swati Srivastava <62814696+swati-sss@users.noreply.github.com>
2026-04-17 14:46:16 -0500

    Update BlueDotVerificationTests.swift

--- Dot/BlueDotVerificationTests.swift
+++ Dot/BlueDotVerificationTests.swift
@@ -1,70 +1,79 @@
 //
-//  SecureKeys.m
-//  WalmartPlatformExtensions
+//  WalmartKeys.swift
+//  Walmart
 //
-//  Created by Venkatesh Jujjavarapu on 01/12/20.
-//  Copyright © 2020 Walmart. All rights reserved.
+//  Created by Alexandre Oliveira Santos on 5/4/21.
+//  Copyright © 2021 Walmart. All rights reserved.
 //
 
-#import "SecureKeys.h"
+import Foundation
+import WalmartPlatform
 
-NSString *const kBrazeAPIKey = @OS_STRINGIFY(BRAZE_KEY);
+/// This represents the actual secure keys used by the Walmart app
+struct WalmartKeys: SecureKeyProviding {
+    let braze = kBrazeAPIKey
+    let pendoKey = "not-needed"
+    let quantumMetricKey = kQuantumMetricKey
+    let quantumMetricStageKey = kQuantumMetricStageKey
+    let appsFlyerDevKey = kAppsFlyerDevKey
+    let appsFlyerAppID = kAppsFlyerAppID
+    let appsFlyerOneLinkAuth = kAppsFlyerOneLinkAuth
+    let flipp = kFlippAPIKey
+    let threatMetrixStgOrgID = kThreatMetrixStgOrgID
+    let threatMetrixProdOrgID = kThreatMetrixProdOrgID
+    let fitPredictorPartnerKey = kFitPredictorPartnerKey
+    let fitPredictorServiceKey = kFitPredictorServiceKey
+    let fitPredictorEmailSalt = kFitPredictorEmailSalt
+    let encryptionKey = kEncryptionKey
+    let idMeClientIdKey = kIdMeClientIdKey
+    let appCenterKey = kAppCenterAPIKey
+    let arAPIClientSecretKey = kArAPIClientSecretKey
+    let arAPIClientIdKey = kArAPIClientIdKey
+    let googleMaps = "not-needed"
+    let fisPayPageId = kFisPaypageIDKey
+    let fisPayPageProdId = kFisPaypageIDProdKey
+    let bazaarvoice = kBazaarvoiceLicenceKey
+    let tslSharedSecrete = ktslSharedSecrete
+    let tslSharedDevSecrete = ktslSharedDevSecrete
+    let tslClientKey = ktslClientKey
+    let tslClientDevKey = ktslClientDevKey
+    let livestreamSaltKey = kLivestreamSaltKey
+    let livestreamSaltDevKey = klivestreamSaltDevKey
+    let aropticalProdKey = karopticalProdKey
+    let aropticalDevKey = karopticalDevKey
+    let dsgNativePartnerKey = kDSGNativePartnerKey
+    let dsgNativeAPIServiceKey = kDSGNativeAPIServiceKey
+    let synchronyStageClientKey = "not-needed"
+    let synchronyStageClientIDKey = "not-needed"
+    let synchronyClientKey = "not-needed"
+    let synchronyClientIDKey = "not-needed"
+    let synchronyBusinessStageClientKey = "not-needed"
+    let synchronyBusinessStageClientID = "not-needed"
+    let synchronyBusinessClientKey = "not-needed"
+    let synchronyBusinessClientID = "not-needed"
+    let compassProdLoggedInConsumerId = kCompassProdLoggedInConsumerIdKey
+    let compassStageLoggedInConsumerId = kCompassStageLoggedInConsumerIdKey
+    let compassProdGuestConsumerId = kCompassProdGuestConsumerIdKey
+    let compassStageGuestConsumerId = kCompassStageGuestConsumerIdKey
+    let compassProdGuestClientSecret = kCompassProdGuestClientSecretKey
+    let compassStageGuestClientSecret = kCompassStageGuestClientSecretKey
+    /// DO NOT REMOVE: Only a decoy, please check docs/security/deceptions.md for more details.
+    let supportSecrets = SupportSecrets()
 
-NSString *const kPerimeterXAPIKey = @OS_STRINGIFY(PERIMETERX_KEY);
+    /// Container
+    let container: Container
 
-NSString *const kPerimeterXAPIDebugKey = @OS_STRINGIFY(PERIMETERX_KEY_DEBUG);
+    init(container: Container) {
+        self.container = container
+    }
 
-NSString *const kQuantumMetricKey = @OS_STRINGIFY(QUANTUMMETRIC_UID);
+    func getPerimeterXKey() -> String {
+        let environmentAPI: EnvironmentAPI = container.get()
 
-NSString *const kQuantumMetricStageKey = @OS_STRINGIFY(QUANTUMMETRIC_STAGE_UID);
+        guard environmentAPI.currentEnvironment.isProduction else {
+            return kPerimeterXAPIDebugKey
+        }
 
-NSString *const kAppsFlyerDevKey = @OS_STRINGIFY(APPSFLYER_DEV_KEY);
-
-NSString *const kAppsFlyerAppID = @OS_STRINGIFY(APPSFLYER_APP_ID);
-
-NSString *const kAppsFlyerOneLinkAuth = @OS_STRINGIFY(APPSFLYER_ONELINK_AUTH);
-
-NSString *const kFitPredictorPartnerKey = @OS_STRINGIFY(FIT_PREDICTOR_PARTNER_KEY);
-
-NSString *const kFitPredictorServiceKey = @OS_STRINGIFY(FIT_PREDICTOR_SERVICE_KEY);
-
-NSString *const kFitPredictorEmailSalt = @OS_STRINGIFY(FIT_PREDICTOR_EMAIL_SALT);
-
-NSString *const kFlippAPIKey = @OS_STRINGIFY(FLIPP_KEY);
-
-NSString *const kThreatMetrixStgOrgID = @OS_STRINGIFY(THREATMETRIX_STG_ORG_ID);
-
-NSString *const kThreatMetrixProdOrgID = @OS_STRINGIFY(THREATMETRIX_PROD_ORG_ID);
-
-NSString *const kEncryptionKey = @OS_STRINGIFY(ENCRYPTION_KEY);
-
-NSString *const kIdMeClientIdKey = @OS_STRINGIFY(ID_ME_CLIENT_ID_KEY);
-
-NSString *const kAppCenterAPIKey = @OS_STRINGIFY(APPCENTER_KEY);
-
-NSString* const kArAPIClientIdKey = @OS_STRINGIFY(AR_API_CLIENT_KEY);
-
-NSString* const kArAPIClientSecretKey = @OS_STRINGIFY(AR_API_CLIENT_SECRET_KEY);
-
-NSString* const kFisPaypageIDKey = @OS_STRINGIFY(FIS_PAYPAGEID_KEY);
-
-NSString* const kFisPaypageIDProdKey = @OS_STRINGIFY(FIS_PAYPAGEID_PROD_KEY);
-
-NSString *const kBazaarvoiceLicenceKey = @OS_STRINGIFY(BAZAARVOICE_KEY);
-
-NSString *const ktslSharedDevSecrete = @OS_STRINGIFY(TSL_SECRETE_DEV);
-NSString *const ktslSharedSecrete = @OS_STRINGIFY(TSL_SECRETE);
-NSString *const ktslClientDevKey = @OS_STRINGIFY(TSL_KEY_DEV);
-NSString *const ktslClientKey = @OS_STRINGIFY(TSL_KEY);
-NSString *const klivestreamSaltDevKey = @OS_STRINGIFY(LIVESTREAM_SALT_DEV);
-NSString *const kLivestreamSaltKey = @OS_STRINGIFY(LIVESTREAM_SALT);
-NSString *const karopticalProdKey = @OS_STRINGIFY(AROPTICAL_PROD);
-NSString *const karopticalDevKey = @OS_STRINGIFY(AROPTICAL_DEV);
-NSString *const kDSGNativePartnerKey = @OS_STRINGIFY(DSG_NATIVE_PARTNER_KEY);
-NSString *const kDSGNativeAPIServiceKey = @OS_STRINGIFY(DSG_NATIVE_PARTNER_SERVICE_KEY);
-NSString *const kCompassProdLoggedInConsumerIdKey = @OS_STRINGIFY(COMPASS_PROD_LOGGED_IN_CONSUMER_ID);
-NSString *const kCompassStageLoggedInConsumerIdKey = @OS_STRINGIFY(COMPASS_STAGE_LOGGED_IN_CONSUMER_ID);
-NSString *const kCompassProdGuestConsumerIdKey = @OS_STRINGIFY(COMPASS_PROD_GUEST_CONSUMER_ID);
-NSString *const kCompassStageGuestConsumerIdKey = @OS_STRINGIFY(COMPASS_STAGE_GUEST_CONSUMER_ID);
-NSString *const kCompassProdGuestClientSecretKey = @OS_STRINGIFY(COMPASS_PROD_GUEST_CLIENT_SECRET);
-NSString *const kCompassStageGuestClientSecretKey = @OS_STRINGIFY(COMPASS_STAGE_GUEST_CLIENT_SECRET);
+        return kPerimeterXAPIKey
+    }
+}
